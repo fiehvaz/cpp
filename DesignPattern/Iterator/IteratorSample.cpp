7f9766ccd39befe240366cd7827082257f8e806f
AtsushiSakai <asakai.amsl+github@gmail.com>
2014-07-26 21:38:38 +0900

    add Desin Pattern sample code directory and iterator pattern

---
+++ DesignPattern/Iterator/IteratorSample.cpp
@@ -0,0 +1,16 @@
+/*
+ * @file: IteratorSample.cpp
+ *
+ * @brief: Iterator model of the Design pattern.
+ *
+ * @author: Atsushi Sakai 
+ *
+ * @lisence:
+ * */
+#include <iostream>
+
+int main(void){
+  std::cout<<"Iterator Sample Start!!"<<std::endl;
+  
+  return 0;
+}
