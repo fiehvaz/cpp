c50af71ecd3d64a2c14420e1ed0689523f2ea002
AtsushiSakai <asakai.amsl+github@gmail.com>
2014-09-20 16:23:13 +0900

    add OPP sample

---
+++ ModernC/OOP/OOP.c
@@ -0,0 +1,23 @@
+/**
+ * @file: OOP.c
+ *
+ * @brief: Object Oriented Programing sample with C
+ *
+ * @author: Atsushi Sakai 
+ *
+ * @copyright (c): 2014 Atsushi Sakai
+ *
+ * @license : GPL Software License Agreement
+ **/
+
+#include <stdio.h>
+
+typedef struct Animal{
+
+}Animal;
+
+int main(void){
+  //cout<<"Factory Method Pattern Sample Start!!"<<endl;
+
+  return 0;
+}
